3c541dd1025e873236502ee9ab557e5351663fc2
smajaas <smajaas@gmail.com>
2022-03-28 18:19:36 +0530

    meme generator

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import { Meme } from '../Meme/Meme';
-import { Switch, Route } from 'react-router-dom'
-import { MemeGenerated } from '../MemeGenerated/MemeGenerated';
+import React from "react";
+import { Meme } from "../Meme/Meme";
+import { Switch, Route } from "react-router-dom";
+import { MemeGenerated } from "../MemeGenerated/MemeGenerated";
 //import styles from './styles.modules.css';
 
 export const App = () => {
   return (
     <div>
-      <h1>Meme Helper</h1>
+      <h1>Meme Generator</h1>
       <Switch>
-      <Route exact path='/'>
-      <Meme />
-      </Route>
-      <Route path='/generated'>
-      <MemeGenerated />
-      </Route>
-    </Switch>
+        <Route exact path="/">
+          <Meme />
+        </Route>
+        <Route path="/generated">
+          <MemeGenerated />
+        </Route>
+      </Switch>
     </div>
-    
-
   );
-    
-}
-
-
+};
